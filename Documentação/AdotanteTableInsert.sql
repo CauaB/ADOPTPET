7e212611fff955e5cfeedc600cc224c9c0ebbba7
Cauã Bandeira Nobre <44434295+CauaB@users.noreply.github.com>
2024-09-14 21:17:54 -0300

    Add files via upload

---
+++ Documentação/AdotanteTableInsert.sql
@@ -0,0 +1,11 @@
+INSERT INTO `adoptpetv1`.`adotante` (`CPF`, `nome`, `telefone`, `sexo`, `endereco`, `quantpets`, `datanasc`, `senha`) VALUES
+('23456789011', 'Carlos Silva', '8765-4321', 'Masculino', 'Avenida das Palmeiras, 456, Cruzeiro', 0, '1990-12-10', 'senha456'),
+('12345678900', 'Ana Souza', '9876-5432', 'Feminino', 'Rua das Orquídeas, 123, Loanda', 0, '1985-08-15', 'senha123'),
+('34567890122', 'Fernanda Oliveira', '7654-3210', 'Feminino', 'Rua dos Lírios, 789, Bau', 0, '1982-05-22', 'senha789'),
+('45678901233', 'João Pereira', '6543-2109', 'Masculino', 'Praça Central, 101, Carneirinhos', 0, '1995-07-14', 'senha012'),
+('56789012344', 'Mariana Costa', '5432-1098', 'Feminino', 'Rua das Palmeiras, 202, Loanda', 0, '1988-09-25', 'senha345'),
+('67890123455', 'Pedro Almeida', '4321-0987', 'Masculino', 'Avenida dos Anjos, 303, Loanda', 0, '1992-03-18', 'senha678'),
+('78901234566', 'Juliana Santos', '3210-9876', 'Feminino', 'Rua do Sol, 404, Cruzeiro', 0, '1985-11-05', 'senha901'),
+('89012345677', 'Lucas Martins', '2109-8765', 'Masculino', 'Rua da Paz, 505, Bau', 0, '1998-06-20', 'senha234'),
+('90123456788', 'Roberta Lima', '1098-7654', 'Feminino', 'Avenida da Liberdade, 606, Carneirinhos', 0, '1989-01-30', 'senha567'),
+('01234567899', 'Thiago Rocha', '0987-6543', 'Masculino', 'Rua da Alegria, 707, Loanda', 0, '1993-04-12', 'senha890');
