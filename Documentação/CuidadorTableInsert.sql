d933de04253300faf1d73bc4692121a4ca5ce981
Cauã Bandeira Nobre <44434295+CauaB@users.noreply.github.com>
2024-09-21 19:00:40 -0300

    Delete Documentação directory

--- Documentação/CuidadorTableInsert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `adoptpetv1`.`cuidador` (`CPF`, `nome`, `telefone`, `senha`, `CODabri`) VALUES
-('87654321011', 'Bruno Rocha', '2345-6789', 'senhaBruno', '1'),
-('98765432100', 'Ana Lima', '1234-5678', 'senhaAna', '1'),
-('76543210922', 'Carla Souza', '3456-7890', 'senhaCarla', '2'),
-('65432109833', 'Daniel Silva', '4567-8901', 'senhaDaniel', '2');
